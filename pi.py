33dbdaf857b3c507c55ceda85727a7b7961babf7
uedaaya <a.ueda.25.4.1706@4nonome.com>
2025-04-30 11:53:23 +0900

    問題１の解答

--- pi.py
+++ pi.py
@@ -1,7 +1,14 @@
-text = """
-    How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
-"""
+text = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
 
 # TODO
 
+transformed_texta = text.replace(",","").replace(".","")
+words = transformed_texta.split()
+wordsa = list(map(len,(words)))
+wordsb = list(map(str,(wordsa)))
+print("".join(wordsb))
+wordsc = "".join(wordsb)
+wordsd = wordsc[0]
+wordse = wordsc[1:]
+wordsf = wordsd +"." + wordse
+print(wordsf)
