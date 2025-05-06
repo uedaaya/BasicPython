75ab62d7eaa4045dc39cf08ffd95f26668b4c98d
uedaaya <a.ueda.25.4.1706@4nonome.com>
2025-05-07 08:51:54 +0900

    制御構文の解答問２

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,14 @@
 from math import sin
 # --example--
 # print(sin(0))
-# >>> 0
+# >>>0 
 # -----------
+import math
+math.pi
+result = 0
+h = math.pi/200
+for number in range(1,101):
+    parts = (h/2)*(sin((number-1)*h)+sin((number)*h))
+    result += parts
+print(result)  
+    
