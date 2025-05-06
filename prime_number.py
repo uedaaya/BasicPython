91a9cbc309ef7e478669a7bf5d6e99236536816f
uedaaya <a.ueda.25.4.1706@4nonome.com>
2025-05-07 08:35:44 +0900

    制御構文の回答問１

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,28 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = input(61)
+b = input(10)
+aa = int(61)
+bb = int(10)
+import math
+j = int(math.sqrt(aa))
+k = int(math.sqrt(bb))
+if aa == 1:
+    print("not prime number")
+elif aa != 1:
+    for i in range(2,j):
+         if aa % i != 0:
+             print("prime number")
+             break
+         else:
+             print("not prime number") 
+if bb == 1:
+    print("not prime number")
+elif bb != 1:
+    for i in range(2,k):
+         if bb % i == 0:
+             print("not prime number")
+             break
+         else:
+             print("prime number")
+
 
 # TODO
