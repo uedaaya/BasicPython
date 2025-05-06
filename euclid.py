6c90fd53b976f47d6940861c289170b3dc310777
uedaaya <a.ueda.25.4.1706@4nonome.com>
2025-05-07 08:45:24 +0900

    制御構文の解答

--- euclid.py
+++ euclid.py
@@ -1,4 +1,25 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+a = 10
+b = 20
 
 # TODO
+while b != 0:
+    a, b = b, a % b
+print(a)
+a = 14
+b = 91
+
+# TODO
+while b != 0:
+    a, b = b, a % b
+print(a)
+a = 91
+b = 14
+
+# TODO
+while b != 0:
+    a, b = b, a % b
+print(a)
+
+
+
+    
