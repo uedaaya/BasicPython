d167ab1c14ee5f553d0a209fa7b71ac0dea81340
uedaaya <a.ueda.25.4.1706@4nonome.com>
2025-05-12 11:18:04 +0900

    関数　素数判定

--- prime_number.py
+++ prime_number.py
@@ -1,28 +1,17 @@
-a = input(61)
-b = input(10)
-aa = int(61)
-bb = int(10)
-import math
-j = int(math.sqrt(aa))
-k = int(math.sqrt(bb))
-if aa == 1:
-    print("not prime number")
-elif aa != 1:
-    for i in range(2,j):
-         if aa % i != 0:
-             print("prime number")
-             break
-         else:
-             print("not prime number") 
-if bb == 1:
-    print("not prime number")
-elif bb != 1:
-    for i in range(2,k):
-         if bb % i == 0:
-             print("not prime number")
-             break
-         else:
-             print("prime number")
-
-
 # TODO
+import math
+def isPrime(n):
+    j = int(math.sqrt(n))
+    if n == 1:
+        return False
+    elif n != 1:
+        for i in range(2,j):
+            if n % i != 0:
+                print("sosuu")
+                return True
+                break
+            else:
+                print("not sosuu")
+                return False
+isPrime(61)
+isPrime(10)
