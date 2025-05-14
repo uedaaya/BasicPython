d39fd14fc36f58533cc67b0eda9a318a5781cfd6
uedaaya <a.ueda.25.4.1706@4nonome.com>
2025-05-14 10:46:36 +0900

    関数解答台形積分

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,3 +1,4 @@
+
 from math import sin
 # --example--
 # print(sin(0))
@@ -5,10 +6,24 @@ from math import sin
 # -----------
 import math
 math.pi
-result = 0
-h = math.pi/200
-for number in range(1,101):
-    parts = (h/2)*(sin((number-1)*h)+sin((number)*h))
-    result += parts
-print(result)  
+def tra_rule(f, a = 0 ,b = 1 ,n = 100):
+    h = (b - a) / n
+    ans = 0
+    for k in range(1, n + 1):
+        x = a + (k - 1) * h
+        F = (h / 2) * (f(x) + f(x + h))
+        ans += F
+        return ans
+
+ans_1 = tra_rule(math.sin, 0 , math.pi / 2 , 50)
+print(ans_1)
+
+ans_2 = tra_rule(lambda x: 4 / (1 + x**2) , 0 , 1 , 100)
+print(ans_2)
+
+ans_3 = tra_rule(lambda x: math.pi ** (1 / 2) * math.exp(-x**2) , -100 , 100 , 1000)
+print(ans_3)
+
+    
+        
     
